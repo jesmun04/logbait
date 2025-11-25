309668c94418c2f93ebeec24cb037f4c6c23810f
Jesús Muñoz Martín-Caro <jesmun04@ucm.es>
2025-11-25 09:29:52 +0100

    Eliminar prototipo de Póker de un jugador
    Fixes #225.

--- src/server/endpoints/protected/ui/juegos/singleplayer/poker/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, Blueprint, render_template
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-
-bp = Blueprint('poker', __name__)
-
-@bp.route('/poker')
-@login_required
-def home():
-    return render_template('pages/casino/juegos/singleplayer/poker.html')
